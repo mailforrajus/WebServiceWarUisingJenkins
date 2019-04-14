89647ba7923922e67a4570fb196ddcb80316f12d
raju <govindh410@gmail.com>
2019-04-14 08:00:43 +0530

    Add existing file

---
+++ src/main/java/com/example/demo/WebserviceExampleApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class WebserviceExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceExampleApplication.class, args);
+	}
+
+	@RequestMapping(value = "/getMessage")
+	public String getMessage() {
+		return "welcome to first Spring Boot Application";
+	}
+
+}
